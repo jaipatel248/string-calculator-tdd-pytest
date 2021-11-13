05607f47341defac7b0c91f7f988157bf6d09147
Jay Patel <imjp248@gmail.com>
2021-11-13 11:00:28 +0530

    Added test case for two number string

--- test/calculate_test.py
+++ test/calculate_test.py
@@ -8,3 +8,7 @@ def test_empty_string():
 def test_single_number():
     assert calculateString("1") == 1
     assert calculateString("12") == 12
+
+def test_two_numbers():
+    assert calculateString("1,2") == 3
+    assert calculateString("12,34") == 46
