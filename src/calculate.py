fbf15e886cf6800fc2a5f3aa80f9e9d8f5c4461f
Jay Patel <imjp248@gmail.com>
2021-11-13 13:34:33 +0530

    Handle : Negative number test case

--- src/calculate.py
+++ src/calculate.py
@@ -1,4 +1,18 @@
 import re
+
+def getNegativeNumbers(numbers):
+    negativeNumbers = []
+    for number in numbers:
+        if number < 0:
+            negativeNumbers.append(number)
+    return ",".join(map(str, negativeNumbers))
+
+def checkNegative(numbers):
+    for number in numbers:
+        if number < 0:
+            raise ValueError("negatives not allowed "+ getNegativeNumbers(numbers))
+    return True
+
 def getNumberList(params):
     delimeters = ",|\n"
     startIndex = 0
@@ -7,7 +21,9 @@ def getNumberList(params):
         delimeters = delimeters + "|" + params[2:startIndex]
         startIndex += 1
     splittedString = re.split(delimeters,params[startIndex:])
-    return list(map(int, splittedString))
+    numbers = list(map(int, splittedString))
+    checkNegative(numbers)
+    return numbers
 
 def calculateString(params):
     if len(params) == 0:
