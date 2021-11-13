5876d097c6b295748a95c33d253de74e930b7d4e
Jay Patel <imjp248@gmail.com>
2021-11-13 11:05:54 +0530

    Handle two numbers test case

--- src/calculate.py
+++ src/calculate.py
@@ -1,4 +1,8 @@
 def calculateString(params):
     if len(params) == 0:
         return 0
-    return int(params)
+    numbers = params.split(",")
+    if len(numbers) == 1:
+        return int(numbers[0])
+    else:
+        return int(numbers[0]) + int(numbers[1])
