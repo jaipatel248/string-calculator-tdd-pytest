8a14f3ee694ff750117e4489d7b40c432968ee60
Jay Patel <imjp248@gmail.com>
2021-11-13 12:43:31 +0530

    Handle : different delimiters test case

--- src/calculate.py
+++ src/calculate.py
@@ -3,6 +3,12 @@ import re
 def calculateString(params):
     if len(params) == 0:
         return 0
-    splittedString = re.split(",|\n",params)
+    delimeters = ",|\n"
+    startIndex = 0
+    if params.startswith('//'):
+        startIndex = params.find('\n')
+        delimeters = delimeters + "|" + params[2:startIndex]
+        startIndex += 1
+    splittedString = re.split(delimeters,params[startIndex:])
     numbers = list(map(int, splittedString))
     return sum(numbers)
