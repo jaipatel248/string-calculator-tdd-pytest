b38fdf6778b1e40f1e5034c9bc781e0e1b3a363d
Jay Patel <imjp248@gmail.com>
2021-11-13 11:25:46 +0530

    Refector : Sum of int list

--- src/calculate.py
+++ src/calculate.py
@@ -3,9 +3,4 @@ def calculateString(params):
         return 0
     splittedString = params.split(",")
     numbers = list(map(int, splittedString))
-    if len(numbers) == 1:
-        return numbers[0]
-    elif len(numbers) == 2:
-        return numbers[0] + numbers[1]
-    else:
-        return sum(numbers)
+    return sum(numbers)
