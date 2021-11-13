0f1ddd41038c2cdbad8c402e539d59633e30be5a
Jay Patel <imjp248@gmail.com>
2021-11-13 10:53:44 +0530

    Added : Test case for single number

--- test/calculate_test.py
+++ test/calculate_test.py
@@ -4,3 +4,7 @@ from src.calculate import calculateString
 
 def test_empty_string():
     assert calculateString("") == 0
+
+def test_single_number():
+    assert calculateString("1") == 1
+    assert calculateString("12") == 12
