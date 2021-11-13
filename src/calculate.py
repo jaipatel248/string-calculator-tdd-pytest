6deef695617a681bb9602aea641621f68e1d8186
Jay Patel <imjp248@gmail.com>
2021-11-13 11:56:25 +0530

    Handle : New lines between numbers test case

--- src/calculate.py
+++ src/calculate.py
@@ -1,6 +1,10 @@
 def calculateString(params):
     if len(params) == 0:
         return 0
-    splittedString = params.split(",")
+    splittedString = []
+    if "\n" in params:
+        splittedString = params.split("\n")
+    else:
+        splittedString = params.split(",")
     numbers = list(map(int, splittedString))
     return sum(numbers)
